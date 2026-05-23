202ced3bb7ebb41b714414fa3b2c52ff69319ba7
Komalamaysaroh21 <komalamaysaroh05@gmail.com>
2026-05-23 12:30:14 +0700

    F

--- app/Http/Controllers/Api/SensorController.php
+++ app/Http/Controllers/Api/SensorController.php
@@ -8,7 +8,24 @@ use App\Models\SensorData;
 
 class SensorController extends Controller
 {
-    ghbgdgdggdg
+    public function store(Request $request)
+    {
+        $request->validate([
+            'cahaya' => 'required|numeric',
+            'kelembaban_tanah' => 'required|numeric',
+            'ph_air' => 'required|numeric',
+        ]);
+
+        $data = SensorData::create([
+            'cahaya' => $request->cahaya,
+            'kelembaban_tanah' => $request->kelembaban_tanah,
+            'ph_air' => $request->ph_air,
+        ]);
+
+        return response()->json([
+            'message' => 'Data berhasil disimpan',
+            'data' => $data
+        ]);
     }
 
     // GET - ambil semua data
